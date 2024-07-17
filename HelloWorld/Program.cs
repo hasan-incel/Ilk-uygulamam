e9fd3743cfcd9b25bef6a93343cfb00db3152109
HASO <HASO@DESKTOP-G86MNHA>
2024-07-17 13:54:34 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hasan İncel");
+Console.WriteLine("Sektör değiştirmek istediğim için Patika Bootcampe başvurdum.");
